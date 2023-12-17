dec603ae5c464aebd57a018a007127a05eea6025
Novikoff <novikoff04@gmail.com>
2023-12-17 18:09:55 +0600

    Change

--- js/logIn.js
+++ js/logIn.js
@@ -1,4 +1,4 @@
-// Display Modal Window
+// Open Modal
 function openModal(modalId) {
     document.getElementById(modalId + 'Modal').style.display = 'block';
     const overlay = document.getElementById('overlay');
@@ -6,7 +6,7 @@ function openModal(modalId) {
     overlay.style.opacity = '1';
 }
 
-// Hide a Modal Window
+// Close Modal
 function closeModal(modalId) {
     document.getElementById(modalId + 'Modal').style.display = 'none';
     const overlay = document.getElementById('overlay');
@@ -14,28 +14,26 @@ function closeModal(modalId) {
     overlay.style.opacity = '0';
 }
 
-// Login simulation
+// Login
 function performLogin() {
-    // Здесь должен быть код отправки данных на сервер (проверки логина и пароля)
     const username = document.getElementById('username').value;
     updateUsername(username);
     closeModal('login');
 }
 
-// Simulated registration
+// Registration
 function performRegistration() {
-    // Здесь должен быть код отправки данных на сервер
     const newUsername = document.getElementById('newUsername').value;
     updateUsername(newUsername);
     closeModal('register');
 }
 
-// Simulation of Exit
+// Log out
 function logout() {
     updateUsername('');
 }
 
-// Обновление отображения имени пользователя
+// Show Username
 function updateUsername(username) {
     const userNames = document.getElementsByClassName('user-name');
 
@@ -44,3 +42,49 @@ function updateUsername(username) {
         userName.style.display = username ? 'inline' : 'none';
     }
 }
+
+
+function saveInformation(username, password) {
+    localStorage.setItem('savedUsername', username);
+    localStorage.setItem('savedPassword', password);
+}
+
+function performLogin() {
+    const savedUsername = localStorage.getItem('savedUsername');
+    const savedPassword = localStorage.getItem('savedPassword');
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === savedUsername && password === savedPassword) {
+        updateUsername(username);
+        closeModal('login');
+    } else {
+        displayErrorMessage('Incorrect username or password', 'login');
+    }
+}
+
+function performRegistration() {
+    const newUsername = document.getElementById('newUsername').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    saveInformation(newUsername, newPassword);
+    closeModal('register');
+}
+
+
+function displayErrorMessage(message, modalId) {
+    const existingErrorMessage = document.querySelector(`#${modalId}Modal .error-message`);
+    
+    if (existingErrorMessage) {
+        existingErrorMessage.remove();
+    }
+
+    const errorMessageContainer = document.createElement('div');
+    errorMessageContainer.className = 'error-message';
+    errorMessageContainer.innerText = message;
+
+    const modal = document.getElementById(modalId + 'Modal');
+    modal.appendChild(errorMessageContainer);
+}
+
